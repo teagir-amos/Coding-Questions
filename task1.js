5253a7012ea03d2b1a5a2453c65b232f6ff2a818
tebagiro amos <114426117+teagir-amos@users.noreply.github.com>
2023-08-22 22:22:12 +0300

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,109 @@
+//Task1. Question1
+let Name = `Amos`
+console.log(Name);
+
+let country = `kenya`;
+console.log(country);
+
+let isMasculine = true || false;
+console.log(isMasculine);
+
+//Task1 .Question2
+let counter = 10;
+counter++;
+counter++;
+counter++;
+counter++;
+counter++;
+let result1 = (counter * 2);
+console.log(result1);
+
+//Task1 .Question3
+let numString = `42`;
+let num = Number(numString);
+let result2 = (num + 10);
+console.log(result2);
+
+//Task1 .Question4
+let result = (`5` + 3);
+console.log(result);
+console.log(typeof result);
+
+//Task1 .Question5
+let myString = `Amos`;
+console.log(myString.length);
+
+let myArray = [`Amos`, `Patrick`, `Ademola`];
+console.log(myArray.length)
+
+//Task1 .Question6
+let a = 1;
+console.log(typeof a);
+let b = `Amos`;
+console.log(typeof b);
+let c = true;
+console.log(typeof c);
+let person = {
+    age: 18,
+    occupation: "nurse",
+}
+console.log(typeof person);
+let gender = "";
+console.log(typeof any);
+
+//Task1 .Question7
+let r = 8;
+let h = 6;
+let area = r * h;
+
+console.log(area);
+
+//Task1 .Question8
+let F = 30;
+const x = (F - 32) * (5 / 9);
+let result3 = x;
+console.log(result3);
+
+//Task1 .Question9
+let w = 3;
+let y = 2;
+let rem = +w % +y
+if (rem = 1) {
+    alert("odd number")
+} else {
+    alert("even number")
+}
+
+//Task1 .Question10
+let num1 = 10;
+if (num1 >= 10) {
+    console.log("greater than 10") // I had to use the if else statement first, before the ternary operator to avoid confusion.
+} else {
+    console.log("less than 10")
+}
+let output = num1 >= 10 ? "greater than 10" : "less than 10";
+console.log(output);
+
+//Task1 .Question11
+let userAge = num = 22;
+let currentYear = 2023;
+let currentAge = currentYear - userAge;
+console.log(currentAge);
+
+//Task1 .Question12
+let num2 = +x;
+let string = "+x + 10";
+console.log(string);
+
+//Task1 .Question13
+let String1 = firstName = "Amos";
+let String2 = lastName = `Akwara`;
+
+console.log(`Good Afternoon` + String1.concat(String2));
+
+//Task1 Question14
+let q = 5 * (3 + 2) / 2;
+let result4 = q;
+console.log(result4);
+
+
